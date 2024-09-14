a8c755720f680be6b3ea1810c47a2ac15307ceb2
programmingtuns <programmingtuns@gamil.com>
2024-09-14 18:18:24 +0600

    react helmet package added

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -1,11 +1,28 @@
 import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { getStoredJobApplication } from "../../utility/localstorage";
-import { list } from "postcss";
+
 
 const AppliedJobs = () => {
   const jobs = useLoaderData();
   const [appliedJobs, setAppliedJobs] = useState([]);
+  const [displyJobs, setDisplayJobs] = useState([]);
+
+  const handleJobsFilter= filter =>{
+    if(filter === 'all'){
+        setDisplayJobs(appliedJobs);
+    }
+    else if (filter === 'remote'){
+        const remoteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Remote');
+        setDisplayJobs(remoteJobs);
+    }
+    else if (filter === 'onsite'){
+        const onsiteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Onsite');
+        setDisplayJobs(onsiteJobs);
+    }
+
+  }
+
   useEffect(() => {
     const storedJobIds = getStoredJobApplication();
     if (jobs.length > 0) {
@@ -17,6 +34,7 @@ const AppliedJobs = () => {
         }
       }
       setAppliedJobs(jobsApplied);
+      setDisplayJobs(jobsApplied);
       // console.log(jobs,storedJobIds,jobsApplied);
     }
   }, []);
@@ -26,19 +44,19 @@ const AppliedJobs = () => {
       <details className="dropdown">
         <summary className="btn m-1">open or close</summary>
         <ul className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
-          <li>
+          <li onClick={()=> handleJobsFilter('all')}>
             <a>All</a>
           </li>
-          <li>
+          <li onClick={()=> handleJobsFilter('remote')}>
             <a>Remote</a>
           </li>
-          <li>
+          <li onClick={()=> handleJobsFilter('onsite')}>
             <a>Onsite</a>
           </li>
         </ul>
       </details>
       <ul>
-        {appliedJobs.map((job) => (
+        {displyJobs.map((job) => (
           <li key={job.id}>
             <span>
               {job.job_title} {job.company_name} {job.remote_or_onsite}
--- src/main.jsx
+++ src/main.jsx
@@ -1,42 +1,42 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Root from './components/Root/Root';
-import Home from './components/Home/Home';
-import AppliedJobs from './components/AppliedJobs/AppliedJobs';
-import ErrorPage from './components/ErrorPage/ErrorPage';
-import JobDetails from './components/JobDetails/JobDetails';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./components/Root/Root";
+import Home from "./components/Home/Home";
+import AppliedJobs from "./components/AppliedJobs/AppliedJobs";
+import ErrorPage from "./components/ErrorPage/ErrorPage";
+import JobDetails from "./components/JobDetails/JobDetails";
+import { HelmetProvider } from "react-helmet-async";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
     errorElement: <ErrorPage></ErrorPage>,
-    children:[
+    children: [
       {
-        path:'/',
-        element: <Home></Home>
+        path: "/",
+        element: <Home></Home>,
       },
       {
-        path:'/applied',
-        element:<AppliedJobs></AppliedJobs>,
-        loader:()=> fetch('/jobs.json') // warning: only load the data you need . do not load all the data
+        path: "/applied",
+        element: <AppliedJobs></AppliedJobs>,
+        loader: () => fetch("/jobs.json"), // warning: only load the data you need . do not load all the data
       },
       {
-        path:'/job/:id',
-        element:<JobDetails></JobDetails>,
-         loader:()=> fetch('../jobs.json') // do not load all data. load only what you need
-      }
-    ]
+        path: "/job/:id",
+        element: <JobDetails></JobDetails>,
+        loader: () => fetch("../jobs.json"), // do not load all data. load only what you need
+      },
+    ],
   },
 ]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <HelmetProvider>
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  </StrictMode>
+);
